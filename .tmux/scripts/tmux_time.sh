3c29b3afeb1a4cda3abbe28ddc3ea17a2779bcf8
Jono Mason <60683297+MassoniMacaroni@users.noreply.github.com>
2025-01-07 18:46:00 +1100

    fix typo in timescript

--- .tmux/scripts/tmux_time.sh
+++ .tmux/scripts/tmux_time.sh
@@ -5,8 +5,8 @@ local_time=$(date +"%H:%M")
 # TZ DB can be found in /usr/share/zoneinfo for quick reference see
 # https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 # Get time in a different timezone (replace with the desired timezone)
-gmt_time=$(TZ="Europe/London" data +"%H:%M")
-edt_time=$(TZ="America/New_York" data +"%H:%M")
+gmt_time=$(TZ="Europe/London" date +"%H:%M")
+edt_time=$(TZ="America/New_York" date +"%H:%M")
 
 # Output both times
 echo "GMT:$gmt_time | EDT:$edt_time"
