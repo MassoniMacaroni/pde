29526c8973419d66e7bc533ea2cdced784fd38d1
Jono Mason <60683297+MassoniMacaroni@users.noreply.github.com>
2025-01-07 18:21:14 +1100

    file required for tmux.conf

---
+++ .tmux/scripts/tmux_time.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Get local time
+local_time=$(date +"%H:%M")
+
+# TZ DB can be found in /usr/share/zoneinfo for quick reference see
+# https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+# Get time in a different timezone (replace with the desired timezone)
+gmt_time=$(TZ="Europe/London" data +"%H:%M")
+edt_time=$(TZ="America/New_York" data +"%H:%M")
+
+# Output both times
+echo "GMT:$gmt_time | EDT:$edt_time"
